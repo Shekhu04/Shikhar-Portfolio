6a7d087b9d6b1de45e4fcacac140035a63c7cbf5
Shekhu04 <gshikhar04@gmail.com>
2025-07-10 20:07:35 +0530

    my portfolio

---
+++ src/constants.js
@@ -0,0 +1,375 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import prismaLogo from './assets/tech_logo/prisma.png';
+import webscoketLogo from './assets/tech_logo/websocket.png';
+import inngestLogo from './assets/tech_logo/inngest.png';
+import trpcLogo from './assets/tech_logo/trpc.png';
+import clerkLogo from './assets/tech_logo/clerk.png';
+import jwtLogo from './assets/tech_logo/jwt.png';
+
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import webflow from './assets/tech_logo/webflow.png'
+import shopify from './assets/tech_logo/shopify.png'
+import shadcn from './assets/tech_logo/shadcn.png'
+import openaiLogo from './assets/tech_logo/openai.png'
+import anthropicLogo from './assets/tech_logo/anthropic.png'
+import grokLogo from './assets/tech_logo/grok.png'
+import e2bLogo from './assets/tech_logo/e2b.png'
+import lovableLogo from './assets/tech_logo/lovable.png'
+import postgreLogo from './assets/tech_logo/postgre.png'
+import jestLogo from './assets/tech_logo/jest.png'
+import cypressLogo from './assets/tech_logo/cypress.png'
+import coderabbitLogo from './assets/tech_logo/coderabbit.png'
+import dockerLogo from './assets/tech_logo/docker.png'
+import awsLogo from './assets/tech_logo/aws.png'
+import pm2Logo from './assets/tech_logo/pm2.png'
+
+
+
+// Experience Section Logo's
+import assignocityLogo from './assets/company_logo/assignocity.png';
+
+// Education Section Logo's
+import dthLogo from './assets/education_logo/dth.png';
+import kccLogo from './assets/education_logo/kcc.png';
+
+// Project Section Logo's
+import vibeLogo from './assets/work_logo/vibe.png';
+import devTinderLogo from './assets/work_logo/devTinder.png';
+import movieFlixLogo from './assets/work_logo/movieFlix.png';
+import posgrestoreLogo from './assets/work_logo/posgrestore.png';
+import whisprLogo from './assets/work_logo/whispr.png';
+import sensaiLogo from './assets/work_logo/sensai.png';
+import netflixLogo from './assets/work_logo/netflix.png';
+
+
+export const SkillsInfo = 
+[
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Webflow', logo: webflow },
+      { name: 'Shopify', logo: shopify },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Shadcn UI', logo: shadcn },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+      { name: 'Prisma', logo: prismaLogo },
+      { name: 'Websocket', logo: webscoketLogo },
+      { name: 'Inngest', logo: inngestLogo },
+      { name: 'tRPC', logo: trpcLogo },
+      { name: 'Clerk', logo: clerkLogo },
+      { name: 'JWT Tokens', logo: jwtLogo },
+
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'AI and Automation',
+    skills: [
+      { name: 'OpenAi', logo: openaiLogo },
+      { name: 'Anthropic', logo: anthropicLogo },
+      { name: 'Grok', logo: grokLogo },
+      { name: 'E2B', logo: e2bLogo },
+      { name: 'Lovable', logo: lovableLogo },
+    ],
+  },
+  {
+    title: 'Quality Assurance Tools',
+    skills: [
+      { name: 'Jest', logo: jestLogo },
+      { name: 'Cypress', logo: cypressLogo },
+      { name: 'CodeRabbit', logo: coderabbitLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Docker', logo: dockerLogo },
+      { name: 'AWS', logo: awsLogo },
+      { name: 'PM2', logo: pm2Logo },
+    ],
+  },
+];
+
+  export const experiences = 
+  [
+    {
+      id: 0,
+      img: assignocityLogo,
+      role: "Frontend Developer Intern",
+      company: "Assignocity - Noida",
+      date: "June 2024 - September 2024",
+      desc: "During my Front-End Developer Internship at Assignocity, I worked on websites using Shopify and Webflow. I helped design and improve the look and feel of 5+ websites, making sure they worked well on all devices and browsers. I also created wireframes and mockups to help the team build faster and more efficiently.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "Wireflow",
+        "Shopify",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: kccLogo,
+      school: "KCC Institutes, Greater Noida",
+      date: "Nov 2021 - July 2025",
+      grade: "7.5 CGPA",
+      desc: "I have completed my B.Tech in Computer Science and Engineering from KCC Institute of Technology and Management, Greater Noida. Throughout my academic journey, I have built a solid foundation in programming, data structures, algorithms, object-oriented programming, database management systems, and software development. I have also actively participated in hackathons, technical events, and workshops, which have helped me strengthen my problem-solving skills and deepen my practical knowledge in full-stack and AI-driven application development.",
+      degree: "Bachelor of Technology - Btech",
+    },
+    {
+      id: 1,
+      img: dthLogo,
+      school: "Dawn The Higher School & College - Prayagraj",
+      date: "Apr 2019 - March 2020",
+      grade: "78%",
+      desc: "I completed my class 12 education from Dawn The Higher School & College, Prayagraj, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM).",
+      degree: "CBSE(XII) - PCM",
+    },
+    {
+      id: 2,
+      img: dthLogo,
+      school: "Dawn The Higher School & College - Prayagraj",
+      date: "Apr 2017 - March 2018",
+      grade: "79%",
+      desc: "I completed my class 10 education from Dawn The Higher School & College, Prayagraj, under the CBSE board, where I studied Science with Computer.",
+      degree: "CBSE(X), Science with Computer Application",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Vibe",
+      description:
+        "Vibe is an AI-first full-stack application generator that allows users to create entire apps and components using natural language prompts. It leverages tools like OpenAI for code generation, Inngest for orchestrating background jobs, and E2B sandboxes with Docker for live previews. The project features Clerk for authentication and billing, a type-safe API layer using tRPC, and a credit-based usage system managed via Prisma and Neon.",
+      image: vibeLogo,
+      tags: [
+        "Next.js",
+        "React JS",
+        "TypeScript",
+        "TailwindCSS",
+        "OpenAI",
+        "tRPC",
+        "Inngest",
+        "E2B",
+        "Docker",
+        "Clerk",
+        "Prisma",
+        "Neon",
+        "AI Integration",
+      ],
+      github: "https://github.com/Shekhu04/vibe",
+      webapp: "https://vibe-rho-tan.vercel.app/",
+    },
+    {
+      id: 1,
+      title: "DevTinder",
+      description:
+        "DevTinder is a full-stack matchmaking platform for developers, supporting over 1,000 users. It features real-time chat using Socket.io, secure payments via Razorpay, and JWT-based authentication. The backend is powered by Node.js, Express.js, and MongoDB, with Redux managing frontend state. The platform includes test coverage using Jest, automated email workflows with cron jobs, and performance-optimized database queries.",
+      image: devTinderLogo,
+      tags: [
+        "React JS",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+        "Redux",
+        "Socket.io",
+        "JWT",
+        "TailwindCSS",
+        "Razorpay",
+        "Jest",
+        "Cypress",
+        "HTML",
+        "CSS",
+        "JavaScript",
+      ],
+      github: "https://github.com/Shekhu04/DevTinder_",
+      webapp: "https://codecrew-frontend-u558.onrender.com/",
+    },
+    {
+      id: 2,
+      title: "MovieFlix",
+      description:
+        "Movie Flix is a dynamic React-based movie application showcasing over 10,000 titles and 2,500+ trailers. It features infinite scrolling, advanced search, and seamless navigation using React Router. Performance is optimized through lazy loading and memoization, resulting in a 60% improvement in load times and a 25% reduction in bounce rate. The state is efficiently managed using Redux Toolkit.",
+      image: movieFlixLogo,
+      tags: [
+        "React JS",
+        "Redux Toolkit",
+        "React Router",
+        "API",
+        "Infinite Scroll",
+        "TailwindCSS",
+        "Lazy Loading",
+        "Memoization",
+        "HTML",
+        "CSS",
+        "JavaScript",
+      ],
+      github: "https://github.com/Shekhu04/MovieFlix",
+      webapp:
+        "https://movie-flix-dxhtaqnyb-shikhar-guptas-projects-b55e997f.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Whispr",
+      description:
+        "Whispr is a real-time chat application built with the MERN stack, Socket.io, TailwindCSS, and Daisy UI. It features JWT-based authentication and authorization, real-time messaging, online user status tracking, and global state management using Zustand. Robust error handling ensures a smooth user experience on both the client and server sides.",
+      image: whisprLogo,
+      tags: [
+        "MERN Stack",
+        "React JS",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+        "Socket.io",
+        "JWT Authentication",
+        "Zustand",
+        "TailwindCSS",
+        "DaisyUI",
+        "Real-Time Chat",
+        "JavaScript",
+      ],
+      github: "https://github.com/Shekhu04/Whispr",
+      webapp: "https://whispr-rzt8.onrender.com/login",
+    },
+    {
+      id: 4,
+      title: "Posgrestore",
+      description:
+        "This is a basic fullstack project I built to explore and gain hands-on experience with **PostgreSQL** and the **PERN stack** (PostgreSQL, Express.js, React, Node.js). It includes authentication, bot detection, rate limiting, and other essential real-world features.",
+      image: posgrestoreLogo,
+      tags: [
+        "PostgreSQL",
+        "Express.js",
+        "React JS",
+        "Node.js",
+        "PERN Stack",
+        "Authentication",
+        "Rate Limiting",
+        "Bot Detection",
+        "REST API",
+        "HTML",
+        "CSS",
+        "JavaScript",
+      ],
+      github: "https://github.com/Shekhu04/PostgreStore",
+      webapp: "https://postgrestore-zgzl.onrender.com/",
+    },
+    {
+      id: 5,
+      title: "Sensai",
+      description:
+        "SENSAI — Your Personalized AI-Powered Career Growth Assistant! Pumped to share one of my most exciting projects yet — an AI career coach platform built to help users grow their careers with personalized AI insights, interview prep, and resume assistance",
+      image: sensaiLogo,
+      tags: [
+        "Next.js 14",
+        "React JS",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Clerk Authentication",
+        "Prisma ORM",
+        "PostgreSQL",
+        "Server Actions",
+        "Inngest",
+        "Gemini API",
+        "AI Integration",
+        "Resume Builder",
+        "Mock Interview",
+        "Career Assistant",
+        "Markdown to PDF",
+        "Cron Jobs",
+        "JavaScript",
+        "TypeScript",
+      ],
+      github: "https://github.com/Shekhu04/sensai",
+      webapp: "https://sensai-eight-kappa.vercel.app/",
+    },
+    {
+      id: 6,
+      title: "NetflixGPT",
+      description:
+        "Netflix GPT is a movie recommendation platform that combines real-time data from TMDB with AI-powered search using Gemini. Built using React, TailwindCSS, and Firebase, it features secure authentication, multilingual browsing, embedded trailers, and a sleek, responsive UI. The app leverages Redux Toolkit, custom hooks, and performance optimizations to deliver a fast, intelligent, and personalized movie experience.",
+      image: netflixLogo,
+      tags: [
+        "React JS",
+        "TailwindCSS",
+        "Firebase",
+        "TMDB API",
+        "Gemini API",
+        "GPT Search",
+        "Redux Toolkit",
+        "useRef",
+        "Memoization",
+        "Custom Hooks",
+        "Authentication",
+        "Multilingual Support",
+        "YouTube Trailer Embed",
+        "Form Validation",
+        "JavaScript",
+        ".env",
+      ],
+      github: "https://github.com/Shekhu04/Netflix-GPT",
+      webapp: "https://netflix-gpt-three-alpha.vercel.app/",
+    }
+  ];  
